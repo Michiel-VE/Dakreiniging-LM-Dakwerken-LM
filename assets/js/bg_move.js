d57913c82c022b3443c62e453cf7374ae5e0ed8a
Michiel-ve <michiel.van.eynde123@gmail.com>
2025-07-08 22:28:18 +0300

    removed bgmove

--- assets/js/bg_move.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("mousemove", (e) => {
-    const x = (e.clientX / window.innerWidth - 0.5) * 10;
-    const y = (e.clientY / window.innerHeight - 0.5) * 10;
-    const bg = document.getElementById("carousel-reiniging");
-    const bg2 = document.getElementById("carousel-herstelling");
-    if (bg) bg.style.transform = `translate(${x}px, ${y}px)`;
-    if (bg2) bg2.style.transform = `translate(${x}px, ${y}px)`;
-});
